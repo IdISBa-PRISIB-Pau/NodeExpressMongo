fb3733df70909bf573ce5862522238cc1003735e
Pau Pericàs <pericassim2002@hotmail.com>
2020-03-19 14:59:22 +0100

    Simple node example

---
+++ NodeJS/node-examples/index.js
@@ -0,0 +1,19 @@
+var rect = {
+	perimeter: (x,y) => (2*(x+y)),
+	area: (x,y) => (x*y)
+};
+
+function solveRect(l, b) {
+	console.log("Resolvent el rectangle l = " + l + " i b = " + b);
+	if ( l <= 0 || b <= 0){
+		console.log("Rectangle dimensions should be greater than 0. l = " + l + " b = " + b)
+	}else{
+		console.log("L'àrea és " + rect.area(l,b));
+		console.log("El perimetre és " + rect.perimeter(l,b));
+	}
+}
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,4);
+solveRect(-1,9);
