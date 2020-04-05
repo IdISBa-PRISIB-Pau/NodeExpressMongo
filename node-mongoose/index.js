d635c8c993806cf0ef7b1f24d9605060966b41cb
Your Name <you@example.com>
2020-04-05 13:22:32 +0200

    mongoose part3 more methods

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,17 +8,34 @@ const connect = mongoose.connect(url);
 
 connect.then((db) => {
 	console.log('Connected to server');
+	
 	Dishes.create({
-        name: 'Uthapizza',
-        description: 'Test'
+        name: 'Uthappizza',
+        description: 'test'
     })
     .then((dish) => {
         console.log(dish);
-        
-        return Dishes.find({}).exec();
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
 
         return Dishes.remove({});
     })
@@ -28,6 +45,7 @@ connect.then((db) => {
     .catch((err) => {
         console.log(err);
     });
+    
 })
 .catch((err) => {
 		console.log(err);
